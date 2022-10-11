d6a1be0e96acaa9fbc036be144aea432ad8d7422
Rasel Parvej <RaselParvejABC@gmail.com>
2022-10-11 11:28:36 +0600

    Banner Done and Responsive

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <header>
+      <h1>Test Refines Your Knowledge.</h1>
+    </header>
+  );
+};
+
+export default Banner;
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -5,6 +5,7 @@ import Topic from "../Topic/Topic";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Banner from "../Banner/Banner";
 
 export async function loader() {
   const response = await fetch("https://openapi.programming-hero.com/api/quiz");
@@ -16,6 +17,7 @@ const Topics = () => {
   const { topics } = useLoaderData();
   return (
     <Container>
+      <Banner />
       <Row className="g-4 align-items-stretch">
         {topics.map((topic) => (
           <Col md={6} lg={4} key={topic.id}>
