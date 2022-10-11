d296405d2353269031e37966406fc6b6d2d6f9c3
Rasel Parvej <RaselParvejABC@gmail.com>
2022-10-11 07:45:36 +0600

    Topics Fetched

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./ErrorPage";
-import Topics from "./components/Topics/Topics";
+import Topics, { loader as topicsLoader } from "./components/Topics/Topics";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        loader: topicsLoader,
         element: <Topics />,
       },
       {
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+export async function loader() {
+  const response = await fetch("https://openapi.programming-hero.com/api/quiz");
+  const responseBody = await response.json();
+  return { topics: responseBody["data"] };
+}
 
 const Topics = () => {
-  return <div>Topics</div>;
+  const { topics } = useLoaderData();
+  return <div>{topics.length}</div>;
 };
 
 export default Topics;
