0abf64d62eadbbae79a6e76412f5935ceced8cd5
Rasel Parvej <RaselParvejABC@gmail.com>
2022-10-11 07:35:55 +0600

    Navbar Done and Responsive; Router Setup Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <span className="fw-bold fs-3">QuizScript</span>
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/">
+                <Nav.Link className="text-center">Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/stat">
+                <Nav.Link className="text-center">Stat</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/blog">
+                <Nav.Link className="text-center">Blog</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./ErrorPage";
+import Topics from "./components/Topics/Topics";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Topics />,
+      },
+      {
+        path: "stat",
+        element: <Topics />,
+      },
+      {
+        path: "blog",
+        element: <Topics />,
+      },
+      {
+        path: "topic/:topicID",
+        element: <Topics />,
+      },
+    ],
   },
 ]);
 
